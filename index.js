c933728d99fa7e35bb25cc84d56f610652985163
AliBaig97 <alibaig97@hotmail.com>
2018-04-10 03:37:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const config = require("./config.json");
-
 client.classList = {
     rogue: {
         name: "Rogue",
