18f54a8de279583a42a192fdadb2e49365cf506c
AliBaig97 <alibaig97@hotmail.com>
2018-09-09 04:11:19 -0500

    Create resume.js

---
+++ commands/resume.js
@@ -0,0 +1,45 @@
+exports.run = (client, message, args) => {
+    
+    if(client.isRunning){
+        return;
+    }
+    
+    if(!client.cypherList[0]) return message.channel.send("Do `~>add` before the cypher starts");
+    
+    client.isRunning = true;
+    
+    message.channel.send("`Cypher Resuming...`");
+    
+    let listStr = "";
+    
+    if(client.cypherList[0])
+    for(var i = 0; i < client.cypherList.length; i++) {
+        listStr += `${i+1}.  ${client.cypherList[i]}\n`;
+    }
+    
+    message.channel.send({embed: {
+        color: 15844367,
+        author: {
+            name: "Cypher Queue",
+            icon_url: client.user.avatarURL
+        },
+        description: listStr
+    }});
+    
+    message.channel.send("`" + client.cypherList[0] + "'s turn!`\n" + "`" + client.time + " seconds` remaining!");
+    
+    client.interval = setInterval( () => {
+        client.time -= 15;
+        // Next persons turn
+        if(client.time == 0){
+            let temp = client.cypherList.shift();
+            client.cypherList.push(temp);
+            client.time = 45;
+            message.channel.send("`" + client.cypherList[0] + "'s turn`\n" + "`" + client.time + " seconds` remaining!");
+        } else {
+            message.channel.send("`" + client.time + " seconds` remaining!");
+        }
+        
+    }, 15000)
+    
+}
