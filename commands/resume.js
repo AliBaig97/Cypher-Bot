00b8e7a5d34aa26b0d828694b445a58bf534c4ef
AliBaig97 <alibaig97@hotmail.com>
2018-09-09 06:45:15 -0500

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -34,7 +34,7 @@ exports.run = (client, message, args) => {
             client.time -= 5;
         
         if(client.time === 5 && client.cypherList[1] !== undefined){
-            message.channel.send("@" + client.cypherList[1] + ", get ready for your turn!");
+            message.channel.send(client.cypherList[1] + ", get ready for your turn!");
         }
         
         // Next persons turn
